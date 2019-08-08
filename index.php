069170ac7fb50897f66a163638382a9dbc0a4335
Awa Melvine <melvineawa@gmail.com>
2019-08-08 07:09:49 +0100

    Incorporate design pages for public section

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>Blog</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <h1 class="logo-text"><span>Awa</span>Inspires</h1>
+    </div>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <!-- <li><a href="#">Sign Up</a></li>
+      <li><a href="#">Login</a></li> -->
+      <li>
+        <a href="#">
+          <i class="fa fa-user"></i>
+          Awa Melvine
+          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+        </a>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#" class="logout">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </header>
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+    <!-- Post Slider -->
+    <div class="post-slider">
+      <h1 class="slider-title">Trending Posts</h1>
+      <i class="fas fa-chevron-left prev"></i>
+      <i class="fas fa-chevron-right next"></i>
+
+      <div class="post-wrapper">
+
+        <div class="post">
+          <img src="assets/images/image_1.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 8, 2019</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/image_1.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 8, 2019</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/image_1.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 8, 2019</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/image_1.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 8, 2019</i>
+          </div>
+        </div>
+
+        <div class="post">
+          <img src="assets/images/image_1.png" alt="" class="slider-image">
+          <div class="post-info">
+            <h4><a href="single.html">One day your life will flash before your eyes</a></h4>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 8, 2019</i>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
+    <!-- // Post Slider -->
+
+    <!-- Content -->
+    <div class="content clearfix">
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <h1 class="recent-post-title">Recent Posts</h1>
+
+        <div class="post clearfix">
+          <img src="assets/images/image_3.png" alt="" class="post-image">
+          <div class="post-preview">
+            <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 11, 2019</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            <a href="single.html" class="btn read-more">Read More</a>
+          </div>
+        </div>
+
+        <div class="post clearfix">
+          <img src="assets/images/image_4.png" alt="" class="post-image">
+          <div class="post-preview">
+            <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 11, 2019</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            <a href="single.html" class="btn read-more">Read More</a>
+          </div>
+        </div>
+        <div class="post clearfix">
+          <img src="assets/images/image_3.png" alt="" class="post-image">
+          <div class="post-preview">
+            <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 11, 2019</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            <a href="single.html" class="btn read-more">Read More</a>
+          </div>
+        </div>
+        <div class="post clearfix">
+          <img src="assets/images/image_3.png" alt="" class="post-image">
+          <div class="post-preview">
+            <h2><a href="single.hmtl">The strongest and sweetest songs yet remain to be sung</a></h2>
+            <i class="far fa-user"> Awa Melvine</i>
+            &nbsp;
+            <i class="far fa-calendar"> Mar 11, 2019</i>
+            <p class="preview-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Exercitationem optio possimus a inventore maxime laborum.
+            </p>
+            <a href="single.html" class="btn read-more">Read More</a>
+          </div>
+        </div>
+
+      </div>
+      <!-- // Main Content -->
+
+      <div class="sidebar">
+
+        <div class="section search">
+          <h2 class="section-title">Search</h2>
+          <form action="index.html" method="post">
+            <input type="text" name="search-term" class="text-input" placeholder="Search...">
+          </form>
+        </div>
+
+
+        <div class="section topics">
+          <h2 class="section-title">Topics</h2>
+          <ul>
+            <li><a href="#">Poems</a></li>
+            <li><a href="#">Quotes</a></li>
+            <li><a href="#">Fiction</a></li>
+            <li><a href="#">Biography</a></li>
+            <li><a href="#">Motivation</a></li>
+            <li><a href="#">Inspiration</a></li>
+            <li><a href="#">Life Lessons</a></li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+    <!-- // Content -->
+
+  </div>
+  <!-- // Page Wrapper -->
+
+  <!-- footer -->
+  <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Awa</span>Inspires</h1>
+        <p>
+          AwaInspires is a fictional blog conceived for the purpose of a tutorial on YouTube.
+          However, Awa has a blog called pieceofadvice.org where he writes truly inspiring stuff.
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 123-456-789</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; info@awainspires.com</span>
+        </div>
+        <div class="socials">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Team</li>
+          </a>
+          <a href="#">
+            <li>Mentores</li>
+          </a>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; codingpoets.com | Designed by Awa Melvine
+    </div>
+  </div>
+  <!-- // footer -->
+
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Slick Carousel -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <!-- Custom Script -->
+  <script src="assets/js/scripts.js"></script>
+
+</body>
+
+</html>
---
+++ single.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <title>Single Post</title>
+</head>
+
+<body>
+  <!-- Facebook Page Plugin SDK -->
+  <div id="fb-root"></div>
+  <script async defer crossorigin="anonymous"
+    src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2&appId=285071545181837&autoLogAppEvents=1">
+  </script>
+
+  <header>
+    <div class="logo">
+      <h1 class="logo-text"><span>Awa</span>Inspires</h1>
+    </div>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <!-- <li><a href="#">Sign Up</a></li>
+      <li><a href="#">Login</a></li> -->
+      <li>
+        <a href="#">
+          <i class="fa fa-user"></i>
+          Awa Melvine
+          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+        </a>
+        <ul>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#" class="logout">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </header>
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+    <!-- Content -->
+    <div class="content clearfix">
+
+      <!-- Main Content Wrapper -->
+      <div class="main-content-wrapper">
+        <div class="main-content single">
+          <h1 class="post-title">This is the title of the Post</h1>
+
+          <div class="post-content">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus animi! Eos!
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae! Ipsa
+              repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat dolores nulla,
+              expedita voluptas officiis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia hic recusandae
+              optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente iste asperiores
+              nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus animi! Eos!
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae! Ipsa
+              repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat dolores nulla,
+              expedita voluptas officiis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia hic recusandae
+              optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente iste asperiores
+              nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus animi! Eos!
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae! Ipsa
+              repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat dolores nulla,
+              expedita voluptas officiis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia hic recusandae
+              optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente iste asperiores
+              nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus animi! Eos!
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae! Ipsa
+              repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat dolores nulla,
+              expedita voluptas officiis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia hic recusandae
+              optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente iste asperiores
+              nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione modi error rerum possimus animi! Eos!
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam placeat at molestias vitae! Ipsa
+              repudiandae praesentium nobis nesciunt, iusto pariatur tenetur commodi! Iste sequi placeat dolores nulla,
+              expedita voluptas officiis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, facere iste! Ex quia hic recusandae
+              optio velit ad consectetur totam sed sunt quasi voluptates, sequi molestias alias sapiente iste asperiores
+              nostrum est voluptatem quae earum accusantium. Totam dolorem possimus rem!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nisi.</p>
+          </div>
+
+        </div>
+      </div>
+      <!-- // Main Content -->
+
+      <!-- Sidebar -->
+      <div class="sidebar single">
+
+        <div class="fb-page" data-href="https://web.facebook.com/codingpoets/" data-small-header="false"
+          data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+          <blockquote cite="https://web.facebook.com/codingpoets/" class="fb-xfbml-parse-ignore"><a
+              href="https://web.facebook.com/codingpoets/">Coding Poets</a></blockquote>
+        </div>
+
+
+        <div class="section popular">
+          <h2 class="section-title">Popular</h2>
+
+          <div class="post clearfix">
+            <img src="assets/images/image_1.png" alt="">
+            <a href="" class="title">
+              <h4>How to overcome your fears</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/image_1.png" alt="">
+            <a href="" class="title">
+              <h4>How to overcome your fears</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/image_1.png" alt="">
+            <a href="" class="title">
+              <h4>How to overcome your fears</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/image_1.png" alt="">
+            <a href="" class="title">
+              <h4>How to overcome your fears</h4>
+            </a>
+          </div>
+          <div class="post clearfix">
+            <img src="assets/images/image_1.png" alt="">
+            <a href="" class="title">
+              <h4>How to overcome your fears</h4>
+            </a>
+          </div>
+
+        </div>
+
+        <div class="section topics">
+          <h2 class="section-title">Topics</h2>
+          <ul>
+            <li><a href="#">Poems</a></li>
+            <li><a href="#">Quotes</a></li>
+            <li><a href="#">Fiction</a></li>
+            <li><a href="#">Biography</a></li>
+            <li><a href="#">Motivation</a></li>
+            <li><a href="#">Inspiration</a></li>
+            <li><a href="#">Life Lessons</a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- // Sidebar -->
+
+    </div>
+    <!-- // Content -->
+
+  </div>
+  <!-- // Page Wrapper -->
+
+  <!-- footer -->
+  <div class="footer">
+    <div class="footer-content">
+
+      <div class="footer-section about">
+        <h1 class="logo-text"><span>Awa</span>Inspires</h1>
+        <p>
+          AwaInspires is a fictional blog conceived for the purpose of a tutorial on YouTube.
+          However, Awa has a blog called pieceofadvice.org where he writes truly inspiring stuff.
+        </p>
+        <div class="contact">
+          <span><i class="fas fa-phone"></i> &nbsp; 123-456-789</span>
+          <span><i class="fas fa-envelope"></i> &nbsp; info@awainspires.com</span>
+        </div>
+        <div class="socials">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+      </div>
+
+      <div class="footer-section links">
+        <h2>Quick Links</h2>
+        <br>
+        <ul>
+          <a href="#">
+            <li>Events</li>
+          </a>
+          <a href="#">
+            <li>Team</li>
+          </a>
+          <a href="#">
+            <li>Mentores</li>
+          </a>
+          <a href="#">
+            <li>Gallery</li>
+          </a>
+          <a href="#">
+            <li>Terms and Conditions</li>
+          </a>
+        </ul>
+      </div>
+
+      <div class="footer-section contact-form">
+        <h2>Contact us</h2>
+        <br>
+        <form action="index.html" method="post">
+          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+          <button type="submit" class="btn btn-big contact-btn">
+            <i class="fas fa-envelope"></i>
+            Send
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+    <div class="footer-bottom">
+      &copy; codingpoets.com | Designed by Awa Melvine
+    </div>
+  </div>
+  <!-- // footer -->
+
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <!-- Slick Carousel -->
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <!-- Custom Script -->
+  <script src="assets/js/scripts.js"></script>
+
+</body>
+
+</html>
