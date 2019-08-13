d3933955171f32e891ed9c3fa0a891aece5d0003
Awa Melvine <melvineawa@gmail.com>
2019-08-13 07:29:03 +0100

    chore(path) define base url and root path and includes

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include("path.php")  ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,30 +21,8 @@
 </head>
 
 <body>
-  <header>
-    <div class="logo">
-      <h1 class="logo-text"><span>Awa</span>Inspires</h1>
-    </div>
-    <i class="fa fa-bars menu-toggle"></i>
-    <ul class="nav">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <!-- <li><a href="#">Sign Up</a></li>
-      <li><a href="#">Login</a></li> -->
-      <li>
-        <a href="#">
-          <i class="fa fa-user"></i>
-          Awa Melvine
-          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-        </a>
-        <ul>
-          <li><a href="#">Dashboard</a></li>
-          <li><a href="#" class="logout">Logout</a></li>
-        </ul>
-      </li>
-    </ul>
-  </header>
+
+  <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
   <!-- Page Wrapper -->
   <div class="page-wrapper">
@@ -211,70 +190,7 @@
   </div>
   <!-- // Page Wrapper -->
 
-  <!-- footer -->
-  <div class="footer">
-    <div class="footer-content">
-
-      <div class="footer-section about">
-        <h1 class="logo-text"><span>Awa</span>Inspires</h1>
-        <p>
-          AwaInspires is a fictional blog conceived for the purpose of a tutorial on YouTube.
-          However, Awa has a blog called pieceofadvice.org where he writes truly inspiring stuff.
-        </p>
-        <div class="contact">
-          <span><i class="fas fa-phone"></i> &nbsp; 123-456-789</span>
-          <span><i class="fas fa-envelope"></i> &nbsp; info@awainspires.com</span>
-        </div>
-        <div class="socials">
-          <a href="#"><i class="fab fa-facebook"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-youtube"></i></a>
-        </div>
-      </div>
-
-      <div class="footer-section links">
-        <h2>Quick Links</h2>
-        <br>
-        <ul>
-          <a href="#">
-            <li>Events</li>
-          </a>
-          <a href="#">
-            <li>Team</li>
-          </a>
-          <a href="#">
-            <li>Mentores</li>
-          </a>
-          <a href="#">
-            <li>Gallery</li>
-          </a>
-          <a href="#">
-            <li>Terms and Conditions</li>
-          </a>
-        </ul>
-      </div>
-
-      <div class="footer-section contact-form">
-        <h2>Contact us</h2>
-        <br>
-        <form action="index.html" method="post">
-          <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
-          <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
-          <button type="submit" class="btn btn-big contact-btn">
-            <i class="fas fa-envelope"></i>
-            Send
-          </button>
-        </form>
-      </div>
-
-    </div>
-
-    <div class="footer-bottom">
-      &copy; codingpoets.com | Designed by Awa Melvine
-    </div>
-  </div>
-  <!-- // footer -->
+  <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
 
   <!-- JQuery -->
--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+<?php include("path.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,30 +21,9 @@
 </head>
 
 <body>
-  <header>
-    <div class="logo">
-      <h1 class="logo-text"><span>Awa</span>Inspires</h1>
-    </div>
-    <i class="fa fa-bars menu-toggle"></i>
-    <ul class="nav">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <!-- <li><a href="#">Sign Up</a></li>
-      <li><a href="#">Login</a></li> -->
-      <li>
-        <a href="#">
-          <i class="fa fa-user"></i>
-          Awa Melvine
-          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-        </a>
-        <ul>
-          <li><a href="#">Dashboard</a></li>
-          <li><a href="#" class="logout">Logout</a></li>
-        </ul>
-      </li>
-    </ul>
-  </header>
+  
+  
+<?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
   <div class="auth-content">
 
@@ -73,7 +53,7 @@
       <div>
         <button type="submit" name="register-btn" class="btn btn-big">Register</button>
       </div>
-      <p>Or <a href="login.html">Sign In</a></p>
+      <p>Or <a href="<?php echo BASE_URL . '/login.php' ?>">Sign In</a></p>
     </form>
 
   </div>
