6db463add1ab3442965da073e32193f0698a11fa
Awa Melvine <melvineawa@gmail.com>
2019-08-16 22:43:01 +0100

    finishe defining select all

--- app/database/db.php
+++ app/database/db.php
@@ -21,26 +21,28 @@ function selectAll($table, $conditions = [])
         $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
         return $records;
     } else {
-        // return records that match conditions ...
-        // $sql = "SELECT * FROM $table WHERE username='Awa' AND admin=1";
-
         $i = 0;
         foreach ($conditions as $key => $value) {
             if ($i === 0) {
-                $sql = $sql . " WHERE $key=$value";
+                $sql = $sql . " WHERE $key=?";
             } else {
-                $sql = $sql . " AND $key=$value";
+                $sql = $sql . " AND $key=?";
             }
             $i++;
         }
-        dd($sql);
-
+        
+        $stmt = $conn->prepare($sql);
+        $values = array_values($conditions);
+        $types = str_repeat('s', count($values));
+        $stmt->bind_param($types, ...$values);
+        $stmt->execute();
+        $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $records;
     }
-    
 }
 
 $conditions = [
-    'admin' => 1,
+    'admin' => 0,
     'username' => 'Awa'
 ];
 
