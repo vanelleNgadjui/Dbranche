ec2c2be3a7d64703fe7661f810fd38c35a1a2b83
Awa Melvine <melvineawa@gmail.com>
2019-08-19 23:09:54 +0100

    Implement select one db function

--- app/database/db.php
+++ app/database/db.php
@@ -11,6 +11,18 @@ function dd($value) // to be deleted
 }
 
 
+function executeQuery($sql, $data)
+{
+    global $conn;
+    $stmt = $conn->prepare($sql);
+    $values = array_values($data);
+    $types = str_repeat('s', count($values));
+    $stmt->bind_param($types, ...$values);
+    $stmt->execute();
+    return $stmt;
+}
+
+
 function selectAll($table, $conditions = [])
 {
     global $conn;
@@ -31,11 +43,7 @@ function selectAll($table, $conditions = [])
             $i++;
         }
         
-        $stmt = $conn->prepare($sql);
-        $values = array_values($conditions);
-        $types = str_repeat('s', count($values));
-        $stmt->bind_param($types, ...$values);
-        $stmt->execute();
+        $stmt = executeQuery($sql, $conditions);
         $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
         return $records;
     }
@@ -58,11 +66,7 @@ function selectOne($table, $conditions)
     }
 
     $sql = $sql . " LIMIT 1";
-    $stmt = $conn->prepare($sql);
-    $values = array_values($conditions);
-    $types = str_repeat('s', count($values));
-    $stmt->bind_param($types, ...$values);
-    $stmt->execute();
+    $stmt = executeQuery($sql, $conditions);
     $records = $stmt->get_result()->fetch_assoc();
     return $records;
 }
