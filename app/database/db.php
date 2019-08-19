fcfbdd2e07b59b561ece606aa0407bf2b4c734db
Awa Melvine <melvineawa@gmail.com>
2019-08-19 20:48:19 +0100

    Implement select one record

--- app/database/db.php
+++ app/database/db.php
@@ -41,10 +41,37 @@ function selectAll($table, $conditions = [])
     }
 }
 
+
+function selectOne($table, $conditions)
+{
+    global $conn;
+    $sql = "SELECT * FROM $table";
+
+    $i = 0;
+    foreach ($conditions as $key => $value) {
+        if ($i === 0) {
+            $sql = $sql . " WHERE $key=?";
+        } else {
+            $sql = $sql . " AND $key=?";
+        }
+        $i++;
+    }
+
+    $sql = $sql . " LIMIT 1";
+    $stmt = $conn->prepare($sql);
+    $values = array_values($conditions);
+    $types = str_repeat('s', count($values));
+    $stmt->bind_param($types, ...$values);
+    $stmt->execute();
+    $records = $stmt->get_result()->fetch_assoc();
+    return $records;
+}
+
+
 $conditions = [
     'admin' => 0,
     'username' => 'Awa'
 ];
 
-$users = selectAll('users', $conditions);
+$users = selectOne('users', $conditions);
 dd($users);
