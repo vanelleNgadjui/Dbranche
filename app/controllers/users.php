984f0389a01615573e630f187af4cecc775698cb
Awa Melvine <melvineawa@gmail.com>
2019-09-02 20:14:10 +0100

    Validate user

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -3,13 +3,34 @@
 include(ROOT_PATH . "/app/database/db.php");
 
 if (isset($_POST['register-btn'])) {
-    unset($_POST['register-btn'], $_POST['passwordConf']);
-    $_POST['admin'] = 0;
+    $errors = array();
 
-    $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    if (empty($_POST['username'])) {
+        array_push($errors, 'Username is required');
+    }
 
-    $user_id = create('users', $_POST);
-    $user = selectOne('users', ['id' => $user_id]);
+    if (empty($_POST['email'])) {
+        array_push($errors, 'Email is required');
+    }
+
+    if (empty($_POST['password'])) {
+        array_push($errors, 'Password is required');
+    }
+
+    if ($_POST['passwordConf'] !== $_POST['password']) {
+        array_push($errors, 'Password do not match');
+    }
+
+    if (count($errors) === 0) {
+        unset($_POST['register-btn'], $_POST['passwordConf']);
+        $_POST['admin'] = 0;
     
-    dd($user);
+        $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    
+        $user_id = create('users', $_POST);
+        $user = selectOne('users', ['id' => $user_id]);
+        
+        dd($user);
+    }
+    
 }
