a5109aeb628c74cec3e61d76014dcefe33f88ae0
Awa Melvine <melvineawa@gmail.com>
2019-09-14 20:20:34 +0100

    Integrate designs for posts and users admin pages

--- app/includes/adminSidebar.php
+++ app/includes/adminSidebar.php
@@ -1,7 +1,7 @@
 <div class="left-sidebar">
     <ul>
-        <li><a href="<?php echo BASE_URL . '/posts/index.php'; ?>">Manage Posts</a></li>
-        <li><a href="<?php echo BASE_URL . '/users/index.php'; ?>">Manage Users</a></li>
-        <li><a href="<?php echo BASE_URL . '/topics/index.php'; ?>">Manage Topics</a></li>
+        <li><a href="<?php echo BASE_URL . '/admin/posts/index.php'; ?>">Manage Posts</a></li>
+        <li><a href="<?php echo BASE_URL . '/admin/users/index.php'; ?>">Manage Users</a></li>
+        <li><a href="<?php echo BASE_URL . '/admin/topics/index.php'; ?>">Manage Topics</a></li>
     </ul>
 </div>
