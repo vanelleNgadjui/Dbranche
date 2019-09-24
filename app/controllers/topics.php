038f8d05eb9bd771d70b0d4c86fe3aed5135861b
AwaMelvine <melvineawa@gmail.com>
2019-09-24 22:27:35 +0100

    Validate topics

--- app/controllers/topics.php
+++ app/controllers/topics.php
@@ -1,9 +1,11 @@
 <?php
 
 include(ROOT_PATH . "/app/database/db.php");
+include(ROOT_PATH . "/app/helpers/validateTopic.php");
 
 $table = 'topics';
 
+$errors = array();
 $id = '';
 $name = '';
 $description = '';
@@ -12,12 +14,19 @@ $topics = selectAll($table);
 
 
 if (isset($_POST['add-topic'])) {
-    unset($_POST['add-topic']);
-    $topic_id = create($table, $_POST);
-    $_SESSION['message'] = 'Topic created successfully';
-    $_SESSION['type'] = 'success';
-    header('location: ' . BASE_URL . '/admin/topics/index.php');
-    exit();
+    $errors = validateTopic($_POST);
+
+    if (count($errors) === 0) {
+        unset($_POST['add-topic']);
+        $topic_id = create($table, $_POST);
+        $_SESSION['message'] = 'Topic created successfully';
+        $_SESSION['type'] = 'success';
+        header('location: ' . BASE_URL . '/admin/topics/index.php');
+        exit(); 
+    } else {
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+    }
 }
 
 
@@ -40,11 +49,20 @@ if (isset($_GET['del_id'])) {
 
 
 if (isset($_POST['update-topic'])) {
-    $id = $_POST['id'];
-    unset($_POST['update-topic'], $_POST['id']);
-    $topic_id = update($table, $id, $_POST);
-    $_SESSION['message'] = 'Topic updated successfully';
-    $_SESSION['type'] = 'success';
-    header('location: ' . BASE_URL . '/admin/topics/index.php');
-    exit();
+    $errors = validateTopic($_POST);
+
+    if (count($errors) === 0) { 
+        $id = $_POST['id'];
+        unset($_POST['update-topic'], $_POST['id']);
+        $topic_id = update($table, $id, $_POST);
+        $_SESSION['message'] = 'Topic updated successfully';
+        $_SESSION['type'] = 'success';
+        header('location: ' . BASE_URL . '/admin/topics/index.php');
+        exit();
+    } else {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+    }
+
 }
