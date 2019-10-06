2e593184d94ebd4bd9ea5ee56d2b6d35bc08b731
AwaMelvine <melvineawa@gmail.com>
2019-10-06 22:32:45 +0100

    Call the create method to create a post

--- app/controllers/posts.php
+++ app/controllers/posts.php
@@ -5,7 +5,16 @@ include(ROOT_PATH . "/app/helpers/validatePost.php");
 
 $topics = selectAll('topics');
 
+$table = 'posts';
+$errors = array();
+
+
 if (isset($_POST['add-post'])) {
     unset($_POST['add-post'], $_POST['topic_id']);
-    dd($_POST);
+    $_POST['user_id'] = 1;
+    $_POST['published'] = 1;
+
+
+    $post_id = create($table, $_POST);
+    dd($post_id);
 }
