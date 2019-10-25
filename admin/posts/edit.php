bfa1b67ccea1218593026f99db01f06ea8e26bfd
AwaMelvine <melvineawa@gmail.com>
2019-10-25 16:50:32 +0100

    Fill post values on edit form

--- admin/posts/edit.php
+++ admin/posts/edit.php
@@ -50,13 +50,13 @@
                     <h2 class="page-title">Edit Post</h2>
 
                     <form action="create.html" method="post">
-                        <div>
+                    <div>
                             <label>Title</label>
-                            <input type="text" name="title" class="text-input">
+                            <input type="text" name="title" value="<?php echo $title ?>" class="text-input">
                         </div>
                         <div>
                             <label>Body</label>
-                            <textarea name="body" id="body"></textarea>
+                            <textarea name="body" id="body"><?php echo $body ?></textarea>
                         </div>
                         <div>
                             <label>Image</label>
@@ -64,10 +64,32 @@
                         </div>
                         <div>
                             <label>Topic</label>
-                            <select name="topic" class="text-input">
-                                <option value="Poetry">Poetry</option>
-                                <option value="Life Lessons">Life Lessons</option>
+                            <select name="topic_id" class="text-input">
+                                <option value=""></option>
+                                <?php foreach ($topics as $key => $topic): ?>
+                                    <?php if (!empty($topic_id) && $topic_id == $topic['id'] ): ?>
+                                        <option selected value="<?php echo $topic['id'] ?>"><?php echo $topic['name'] ?></option>
+                                    <?php else: ?>
+                                        <option value="<?php echo $topic['id'] ?>"><?php echo $topic['name'] ?></option>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+
                             </select>
+                        </div>
+                        <div>
+                            <?php if (empty($published) && $published == 0): ?>
+                                <label>
+                                    <input type="checkbox" name="published">
+                                    Publish
+                                </label>
+                            <?php else: ?>
+                                <label>
+                                    <input type="checkbox" name="published" checked>
+                                    Publish
+                                </label>
+                            <?php endif; ?>
+                           
+
                         </div>
                         <div>
                             <button type="submit" class="btn btn-big">Update Post</button>
