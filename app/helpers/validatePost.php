f4fd406a1fdccb8ad45f2f38fd54b6cd1b3512e1
AwaMelvine <melvineawa@gmail.com>
2019-11-15 23:02:37 +0100

    Fix update validation issue

--- app/helpers/validatePost.php
+++ app/helpers/validatePost.php
@@ -19,7 +19,13 @@ function validatePost($post)
 
     $existingPost = selectOne('posts', ['title' => $post['title']]);
     if ($existingPost) {
-        array_push($errors, 'Post with that title already exists');
+        if (isset($post['update-post']) && $existingPost['id'] != $post['id']) {
+            array_push($errors, 'Post with that title already exists');
+        }
+
+        if (isset($post['add-post'])) {
+            array_push($errors, 'Post with that title already exists');
+        }
     }
 
     return $errors;
